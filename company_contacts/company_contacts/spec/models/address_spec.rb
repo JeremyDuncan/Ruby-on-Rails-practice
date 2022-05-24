bcd9142cdedf80e1f223afa828860ae38ce65339
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-24 17:13:44 -0400

    Created rspec test

--- company_contacts/company_contacts/spec/models/address_spec.rb
+++ company_contacts/company_contacts/spec/models/address_spec.rb
@@ -21,6 +21,13 @@ RSpec.describe Address, type: :model do
     test_address = new_user.addresses.create street_name: 'Coder Lane', street_number: 1, state: 'GA', zip: 30_313
     expect(test_address.errors[:city]).to_not be_empty
   end
+
+  it 'is not valid without street_name' do
+    new_user = Account.create username: 'jduncan', password: 'Password123', email: 'jeremy.duncan1984@gmail.com'
+
+    test_address = new_user.addresses.create street_name: 'Coder Lane', street_number: 1, city: 'Atlanta', zip: 30_313
+    expect(test_address.errors[:street_name]).to_not be_empty
+  end
 end
 
 # it 'is not valid without street_number' do
