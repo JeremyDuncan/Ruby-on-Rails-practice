1226a0d552d927857dfdbdbf7b9721da70575572
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-24 17:12:30 -0400

    Created validates for city

--- company_contacts/company_contacts/app/models/address.rb
+++ company_contacts/company_contacts/app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :account
-  validates :street_number, :street_name, presence: true
+  validates :street_number, :street_name, :city, presence: true
 end
