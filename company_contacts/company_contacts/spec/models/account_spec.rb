ce75f07e1650c8ea0ea5e26881c98bc6d11e1de4
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-24 14:06:58 -0400

    Created rspec test

--- company_contacts/company_contacts/spec/models/account_spec.rb
+++ company_contacts/company_contacts/spec/models/account_spec.rb
@@ -26,4 +26,9 @@ RSpec.describe Account, type: :model do
     test2 = Account.create password: 'password123', email: 'jeremy.duncan1984@gmail.com'
     expect(test2.errors[:username]).to_not be_empty
   end
+
+  it 'password must contain at least 6 characters' do
+    test = Account.create password: 'pass', email: 'jeremy.duncan1984@gmail.com'
+    expect(test.errors[:password]).to_not be_empty
+  end
 end
