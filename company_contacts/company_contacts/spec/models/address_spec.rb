4896afd3b1f776e4cb08651b363253f48a2484cc
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-24 16:17:04 -0400

    Created rspec test for Address

--- company_contacts/company_contacts/spec/models/address_spec.rb
+++ company_contacts/company_contacts/spec/models/address_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  it 'is not valid without street_name' do
-    test = Address.create street_number: 123, street_name: 'coder lane', city: 'Atlanta', state: 'GA', zip: 30_031
-    expect(test.errors[:street_number]).to_not be_empty
+  it 'is not valid without street_number' do
+    new_user = Account.create username: 'jduncan', password: 'Password123', email: 'jeremy.duncan1984@gmail.com'
+
+    test_address = new_user.addresses.create street_name: 'coder lane', city: 'Atlanta', state: 'GA', zip: 30_313
+    expect(test_address.errors[:street_number]).to_not be_empty
   end
 end
+
+# it 'is not valid without street_number' do
+#   test = Account.create username: 'jduncan', password: 'Password123', email: 'jeremy.duncan1984@gmail.com'
+
+#   test.addresses.create street_number: 123, street_name: 'coder lane', city: 'Atlanta', state: 'GA', zip: 30_313
+
+#   expect(test.errors[:street_number]).to_not be_empty
+# end
+
+# expect(act[:street_name]).to eq 'coder lane'
