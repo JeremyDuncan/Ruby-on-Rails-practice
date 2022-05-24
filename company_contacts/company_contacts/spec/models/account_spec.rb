d73a0d8c951fba5115f0f0af94d2926c205c2fa0
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-24 14:21:11 -0400

    Created rspec test

--- company_contacts/company_contacts/spec/models/account_spec.rb
+++ company_contacts/company_contacts/spec/models/account_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Account, type: :model do
   it 'is not valid without a username' do
-    test = Account.create password: 'password123', email: 'jeremy.duncan1984@gmail.com'
+    test = Account.create  password: 'password123', email: 'jeremy.duncan1984@gmail.com'
     expect(test.errors[:username]).to_not be_empty
   end
 
@@ -17,24 +17,24 @@ RSpec.describe Account, type: :model do
   end
 
   it 'username must contain at least 5 characters' do
-    test = Account.create username: 'jdun'
+    test = Account.create username: 'jdun', password: 'Password123', email: 'jeremy.duncan1984@gmail.com'
     expect(test.errors[:username]).to_not be_empty
   end
 
   it 'username must be unique' do
-    test = Account.create password: 'password123', email: 'jeremy.duncan1984@gmail.com'
-    test2 = Account.create password: 'password123', email: 'jeremy.duncan1984@gmail.com'
+    test = Account.create username: 'jduncan', password: 'Password123', email: 'jeremy.duncan1984@gmail.com'
+    test2 = Account.create username: 'jduncan', password: 'Password123', email: 'jeremy.duncan1984@gmail.com'
     expect(test2.errors[:username]).to_not be_empty
   end
 
   it 'password must contain at least 6 characters' do
-    test = Account.create password: 'pass', email: 'jeremy.duncan1984@gmail.com'
+    test = Account.create username: 'jduncan', password: 'Pass', email: 'jeremy.duncan1984@gmail.com'
     expect(test.errors[:password]).to_not be_empty
   end
 
   it 'password must be unique' do
-    test = Account.create password: 'password123', email: 'jeremy.duncan1984@gmail.com'
-    test2 = Account.create password: 'password123', email: 'jeremy.duncan1984@gmail.com'
+    test = Account.create username: 'jduncan', password: 'PassWord1234', email: 'jeremy.duncan1984@gmail.com'
+    test2 = Account.create username: 'jduncan', password: 'PassWord1234', email: 'jeremy.duncan1984@gmail.com'
     expect(test2.errors[:password]).to_not be_empty
   end
 end
