c0f73e9845e3858f4a9b6682714fe115a5de943a
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-24 14:09:56 -0400

    Created validates for password length

--- company_contacts/company_contacts/app/models/account.rb
+++ company_contacts/company_contacts/app/models/account.rb
@@ -2,4 +2,5 @@ class Account < ApplicationRecord
   validates :username, :password, :email, presence: true
   validates :username, length: { minimum: 5 }
   validates :username, uniqueness: true
+  validates :password, length: { minimum: 6 }
 end
