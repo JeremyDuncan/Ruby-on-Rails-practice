9c3aa797ce17f4a938e9854cb7c806f42a272968
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-06-01 19:57:58 -0400

    completed more assignments

--- routes_controllers_views_params/app/controllers/main_controller.rb
+++ routes_controllers_views_params/app/controllers/main_controller.rb
@@ -1,6 +1,11 @@
 class MainController < ApplicationController
   def answer
-    render html: 'this is the answer.'
+    @result_string = if params[:number].to_i.even?
+                       'Even'
+                     else
+                       'Odd'
+                     end
+    render 'answer'
   end
 
   def question
@@ -15,5 +20,40 @@ class MainController < ApplicationController
   def greeting
     @name = params[:name]
     render 'greeting'
+  end
+
+  def jeremyduncan
+    render 'jeremy'
+  end
+
+  def cubed
+    @number = params[:num]
+    @result = params[:num].to_i**3
+    render 'cubed'
+  end
+
+  def evenly
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+
+    @result = if @num1 % @num2 == 0
+                'Evenly divisible'
+              else
+                'not evenly divisible'
+              end
+
+    render 'evenly'
+  end
+
+  def palindrome
+    @string = params[:string]
+
+    if @string == @string.reverse
+      @result = 'is a palindrome.'
+    else
+      @result = 'is not a palindrome.'
+
+      render 'palindrome'
+    end
   end
 end
--- routes_controllers_views_params/config/routes.rb
+++ routes_controllers_views_params/config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'main#home'
-  get '/answer' => 'main#answer'
+  get '/answer/:number' => 'main#answer'
   get '/question' => 'main#question'
   get '/greeting/:name' => 'main#greeting'
+  get '/jeremyduncan' => 'main#jeremyduncan'
+  get '/cubed/:num' => 'main#cubed'
+  get '/evenly/:num1+:num2' => 'main#evenly'
+  get '/palindrome/:string' => 'main#palindrome'
 end
